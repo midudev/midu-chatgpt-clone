467e394a891be6192a445428de6f60217edcbcf1
Miguel Ángel Durán <miduga@gmail.com>
2023-03-02 12:48:38 +0100

    Use separated components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,139 +1,36 @@
-import { Avatar } from '@/components/Avatar.jsx'
-import { ChatGPTLogo, PlusIcon, SendIcon } from '@/components/Icons.jsx'
-import { TypingEffect } from '@/components/TypingEffect.jsx'
-import { useMessageStore } from '@/store/messages.js'
-import Head from 'next/head'
-import { useRef } from 'react'
-
-function Layout({ children }) {
-  return (
-    <>
-      <Head>
-        <title>midu Chat GPT</title>
-      </Head>
-      <div className='w-full relative bg-gptgray h-screen'>
-        <Aside />
-        {children}
-      </div>
-    </>
-  )
-}
-
-function Aside() {
-  return (
-    <aside className='bg-gptdarkgray fixed flex w-64 h-screen flex-col'>
-      <nav className='flex flex-col flex-1 h-full p-2 space-y-1'>
-        <button className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-2 flex-shrink-0 border border-white/20'>
-          <PlusIcon />
-          New chat
-        </button>
-      </nav>
-    </aside>
-  )
-}
-
-function UserAvatar() {
-  return (
-    <img
-      alt='Foto de midu'
-      src='https://pbs.twimg.com/profile_images/1613612257015128065/oA0Is67J_400x400.jpg'
-    />
-  )
-}
-
-function Message({ ia, message }) {
-  const avatar = ia ? <ChatGPTLogo /> : <UserAvatar />
-  const textElement = ia ? <TypingEffect text={message} /> : message
-
-  return (
-    <div className={`text-gray-100 ${ia ? 'bg-gptlightgray' : 'bg-gptgray'}`}>
-      <article className='flex gap-4 p-6 m-auto max-w-3xl'>
-        <Avatar>{avatar}</Avatar>
-
-        <div className='min-h-[20px] flex flex-1 flex-col items-start gap-4 whitespace-pre-wrap'>
-          <div className='prose-invert w-full break-words'>
-            <p>{textElement}</p>
-          </div>
-        </div>
-      </article>
-    </div>
-  )
-}
+import { Banner } from '@/components/Banner'
+import { ChatForm } from '@/components/CreatePrompt'
+import { Layout } from '@/components/Layout'
+import { Message } from '@/components/Message'
+import { Welcome } from '@/components/Welcome'
+import { useConversationsStore } from '@/store/conversations.js'
 
 function Chat() {
-  const messages = useMessageStore((state) => state.messages)
+  const selectedConversation = useConversationsStore(
+    (state) => state.selectedConversation
+  )
+  const messages = useConversationsStore((state) => {
+    const { selectedConversation } = state
+    return state.conversationsMessages[selectedConversation]
+  })
+
+  const renderContent = () => {
+    if (!selectedConversation) return <Welcome />
+    return messages?.map((entry) => <Message key={entry.id} {...entry} />)
+  }
 
   return (
-    <div className='flex flex-col h-full flex-1 pl-64'>
-      <main>
-        {messages.map((entry) => (
-          <Message key={entry.id} {...entry} />
-        ))}
-      </main>
+    <div className='flex flex-col flex-1 h-full pl-64'>
+      <main>{renderContent()}</main>
       <ChatForm />
     </div>
-  )
-}
-
-function ChatForm() {
-  const sendPrompt = useMessageStore((state) => state.sendPrompt)
-  const textAreaRef = useRef()
-
-  const handleSubmit = (event) => {
-    event?.preventDefault()
-    const { value } = textAreaRef.current
-    console.log({ value })
-    sendPrompt({ prompt: value })
-    textAreaRef.current.value = ''
-  }
-
-  const handleChange = () => {
-    const el = textAreaRef.current
-
-    el.style.height = '0px'
-    const scrollHeight = el.scrollHeight
-    el.style.height = scrollHeight + 'px'
-  }
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault()
-      handleSubmit()
-    }
-  }
-
-  return (
-    <section className='absolute bottom-0 w-full left-0 right-0 ml-32'>
-      <form
-        onSubmit={handleSubmit}
-        onKeyDown={handleKeyDown}
-        className='flex flex-row max-w-3xl pt-6 m-auto mb-6'
-      >
-        <div className='relative flex flex-col flex-grow w-full px-4 py-3 text-white border rounded-md shadow-lg bg-gptlightgray border-gray-900/50'>
-          <textarea
-            onChange={handleChange}
-            ref={textAreaRef}
-            rows={1}
-            tabIndex={0}
-            autoFocus
-            defaultValue=''
-            className='w-full h-[24px] resize-none bg-transparent m-0 border-0 outline-none'
-          />
-          <button
-            type='submit'
-            className='absolute p-1 rounded-md bottom-2.5 right-2.5'
-          >
-            <SendIcon />
-          </button>
-        </div>
-      </form>
-    </section>
   )
 }
 
 export default function Home() {
   return (
     <Layout>
+      <Banner />
       <Chat />
     </Layout>
   )
