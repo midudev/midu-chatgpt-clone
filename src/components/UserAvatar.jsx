0a0b1a580ca692ee647bf52011a2ceb8914f53fd
Miguel Ángel Durán <miduga@gmail.com>
2023-03-02 12:48:17 +0100

    Separate UserAvatar

---
+++ src/components/UserAvatar.jsx
@@ -0,0 +1,8 @@
+export function UserAvatar() {
+  return (
+    <img
+      alt='Foto de midu'
+      src='https://pbs.twimg.com/profile_images/1613612257015128065/oA0Is67J_400x400.jpg'
+    />
+  )
+}
