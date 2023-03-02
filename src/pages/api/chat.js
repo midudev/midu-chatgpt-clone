737332533fd48aa989044edc361d42c9aad44b52
Miguel Ángel Durán <miduga@gmail.com>
2023-03-02 12:49:19 +0100

    Use new Chat API

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,48 +1,118 @@
-// Aquí añade tu API_KEY, porque esta no funcionará
-const OPENAI_API_KEY = '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'
+import { decompress } from 'lz-ts'
+
+const { OPENAI_API_KEY } = process.env
+
+const date = new Date()
+const currentDate = `${date.getFullYear()}-${
+  date.getMonth() + 1
+}-${date.getDate()}`
+
+const INITIAL_ROLE_MESSAGE = {
+  role: 'system',
+  content: `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Current date is: ${currentDate}.`
+}
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end()
+  if (req.method !== 'GET') return res.status(405).end()
 
-  const { prompt } = req.body
+  const { prompt, conversation } = req.query
 
   if (!prompt) {
     return res.status(400).json({ error: 'Prompt is required' })
   }
 
+  let previousConversation = []
   try {
-    console.log('antes de la response')
-    const response = await fetch('https://api.openai.com/v1/completions', {
+    const decompressedConversation = decompress(conversation)
+    const parsedConversation = JSON.parse(decompressedConversation)
+
+    previousConversation = parsedConversation
+      .map((entry) => {
+        console.log(entry)
+        const role = entry.ia ? 'assistant' : 'user'
+
+        // ignore messages without content
+        if (!entry.message) return null
+
+        return {
+          role,
+          content: entry.message
+        }
+      })
+      .filter(Boolean)
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${OPENAI_API_KEY}`
       },
       body: JSON.stringify({
-        model: 'text-davinci-003',
-        prompt: `Responde como si fueras la inteligencia artificial conversacional ChatGPT. El usuario te escribe un prompt y tú debes contestar de forma natural. El prompt es:\n\n${prompt}`,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
+        model: 'gpt-3.5-turbo-0301',
+        stream: true,
+        messages: [
+          INITIAL_ROLE_MESSAGE,
+          ...previousConversation,
+          {
+            role: 'user',
+            content: prompt
+          }
+        ]
       })
     })
-
-    console.log('antes del check')
 
     if (!response.ok) {
       console.error(response.statusText)
       return res.status(500).json({ error: 'OpenAI API error' })
     }
 
-    console.log('despues del check')
+    res.writeHead(200, {
+      'Access-Control-Allow-Origin': '*',
+      Connection: 'keep-alive',
+      'Content-Encoding': 'none',
+      'Cache-Control': 'no-cache, no-transform',
+      'Content-Type': 'text/event-stream;charset=utf-8'
+    })
 
-    const json = await response.json()
+    const reader = response.body.getReader()
+    const decoder = new TextDecoder('utf-8')
 
-    console.log(json)
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) {
+        res.end('data: [DONE]\n\n')
+        break
+      }
 
-    return res.status(200).json({ response: json.choices[0].text.trim() })
+      const text = decoder.decode(value)
+      const data = text
+        .split('\n')
+        .filter(Boolean)
+        .map((line) => line.trim().replace('data: ', '').trim())
+
+      for (const line of data) {
+        if (line === '[DONE]') {
+          res.end('data: [DONE]\n\n')
+          break
+        }
+
+        let content = ''
+        try {
+          const json = JSON.parse(line)
+          content = json?.choices[0]?.delta?.content ?? ''
+        } catch (e) {
+          console.error('No se pudo parsear la línea', line)
+          console.error(e)
+        }
+
+        res.write(`data: ${content}\n\n`)
+        res.flush()
+      }
+    }
   } catch (e) {
     console.error(e)
     res.status(500).json({ error: e })
