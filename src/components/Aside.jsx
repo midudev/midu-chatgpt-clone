b15fabd1db5137cae40c69e0c8d19c0d9f1e570d
Miguel Ángel Durán <miduga@gmail.com>
2023-03-03 09:47:39 +0100

    Add new buttons

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,8 +1,15 @@
 import { useConversationsStore } from '@/store/conversations'
 import { MessageIcon, PencilIcon, PlusIcon, TrashIcon } from './Icons'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export function Aside() {
-  const conversationsInfo = useConversationsStore(state => state.conversationsInfo)
+  const conversationsInfo = useConversationsStore(
+    (state) => state.conversationsInfo
+  )
+  const removeConversation = useConversationsStore(
+    (state) => state.removeConversation
+  )
+  const [animationParent] = useAutoAnimate()
 
   return (
     <aside className='fixed flex flex-col w-64 h-screen bg-gptdarkgray'>
@@ -13,28 +20,44 @@ export function Aside() {
         </button>
 
         <div className='flex-col flex-1 overflow-y-auto border-b border-white/20'>
-          <div className='flex flex-col gap-2 text-sm text-gray-100'>
-            {
-              Object.entries(conversationsInfo).map(([key, conversationInfo]) => (
-                <a key={key} className='relative flex items-center gap-3 px-3 py-3 break-all bg-gray-800 rounded-md cursor-pointer pr-14 hover:bg-gray-800 group'>
+          <div
+            ref={animationParent}
+            className='flex flex-col gap-2 text-sm text-gray-100'
+          >
+            {Object.entries(conversationsInfo).map(
+              ([key, conversationInfo]) => (
+                <a
+                  key={key}
+                  className='relative flex items-center gap-3 px-3 py-3 break-all bg-gray-800 rounded-md cursor-pointer pr-14 hover:bg-gray-800 group'
+                >
                   <MessageIcon />
                   <div className='relative flex-1 overflow-hidden break-all text-ellipsis max-h-5'>
                     {conversationInfo.name}
                     <div className='absolute inset-y-0 right-0 z-10 w-8 bg-gradient-to-l from-gray-800' />
                   </div>
+
                   <div className='absolute z-10 flex visible text-gray-300 right-1'>
                     <button className='p-1 hover:text-white'>
                       <PencilIcon />
                     </button>
-                    <button className='p-1 hover:text-white'>
+
+                    <button
+                      onClick={() => removeConversation({ id: key })}
+                      className='p-1 hover:text-white'
+                    >
                       <TrashIcon />
                     </button>
                   </div>
                 </a>
-              ))
-            }
+              )
+            )}
           </div>
+
         </div>
+        <a className='flex items-center gap-3 px-3 py-3 text-sm text-white transition-colors duration-200 rounded-md cursor-pointer hover:bg-gray-500/10'>
+          <TrashIcon />
+          Clear conversations
+        </a>
       </nav>
     </aside>
   )
