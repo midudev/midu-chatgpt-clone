66917b3f0d91ceb69410dbb4a94f496b9621dbfc
Miguel Ángel Durán <miduga@gmail.com>
2023-03-06 21:27:21 +0100

    Add Scroll to Bottom

--- src/components/ScrollToBottom.jsx
+++ src/components/ScrollToBottom.jsx
@@ -1,7 +1,7 @@
 export function ScrollToBottom () {
   return (
-    <button class='cursor-pointer absolute right-6 bottom-[124px] md:bottom-[120px] z-10 rounded-full border border-gray-200 bg-gray-50 text-gray-600 dark:border-white/10 dark:bg-white/10 dark:text-gray-200'>
-      <svg stroke='currentColor' fill='none' stroke-width='2' viewBox='0 0 24 24' stroke-linecap='round' stroke-linejoin='round' class='h-4 w-4 m-1' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'><line x1='12' y1='5' x2='12' y2='19' /><polyline points='19 12 12 19 5 12' /></svg>
+    <button className='cursor-pointer absolute right-6 bottom-[124px] md:bottom-[120px] z-10 rounded-full border border-gray-200 bg-gray-50 text-gray-600 dark:border-white/10 dark:bg-white/10 dark:text-gray-200'>
+      <svg stroke='currentColor' fill='none' strokeWidth='2' viewBox='0 0 24 24' strokeLinecap='round' strokeLinejoin='round' className='w-4 h-4 m-1' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'><line x1='12' y1='5' x2='12' y2='19' /><polyline points='19 12 12 19 5 12' /></svg>
     </button>
   )
 }
