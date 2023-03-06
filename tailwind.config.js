38db03278c8f89db34450f14b7cd699a56b3c182
Miguel Ángel Durán <miduga@gmail.com>
2023-03-06 21:23:47 +0100

    Add gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       animation: {
         typing: 'blink 1s steps(5, start) infinite'
       },
+      backgroundImage: {
+        gradient: 'linear-gradient(180deg,rgba(53,55,64,0),#353740 58.85%)'
+      },
       keyframes: {
         blink: {
           to: { visibility: 'hidden' }
