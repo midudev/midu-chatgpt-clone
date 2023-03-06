58d3b09b1259a1e7823818f532a21ef8bf371a71
Miguel Ángel Durán <miduga@gmail.com>
2023-03-06 21:25:05 +0100

    Add Welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,26 +1,59 @@
-import { GitHubIcon, ReactIcon, TwitchIcon } from './Icons'
+import { GitHubIcon, ReactIcon, SunIcon, TwitchIcon } from './Icons'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { useConversationsStore } from '@/store/conversations'
+
+const EXAMPLES = [
+  'Explain quantum computing in simple terms',
+  'Got any creative ideas for a 10 year old’s birthday?',
+  'How do I make an HTTP request in Javascript?'
+]
 
 export function Welcome() {
+  const [animationParent] = useAutoAnimate()
+  const sendPrompt = useConversationsStore(state => state.sendPrompt)
+
   return (
-    <section className='flex justify-center w-full m-auto'>
-      <div class='text-gray-800 w-full md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col px-6 dark:text-gray-100'>
-        <h1 class='text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-4 flex gap-2 items-center justify-center'>
+    <section
+      ref={animationParent}
+      className='flex justify-center w-full m-auto'
+    >
+      <div className='w-full px-6 text-gray-800 md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col dark:text-gray-100'>
+        <h1 className='text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 flex gap-2 items-center justify-center'>
           miduGPT
         </h1>
 
-        <p class='text-center max-w-md m-auto block align-middle mb-8'>
-          Esta página <strong>no es la oficial de ChatGPT</strong>. Es un clon creado con React <ReactIcon className='inline-block w-6 h-6' /> y Tailwind para fines educativos.
+        <p className='block max-w-md m-auto mb-10 text-center align-middle'>
+          Esta página <strong>no es la oficial de ChatGPT</strong>. Es un clon
+          creado con React <ReactIcon className='inline-block w-6 h-6' /> y
+          Tailwind para fines educativos.
         </p>
 
-        <div class='flex gap-x-2 max-w-xl m-auto'>
-          <a className='flex items-center justify-center w-[250px] p-2 font-bold text-white bg-purple-700 rounded-full gap-x-2 hover:bg-purple-500 focus:border-purple-800 active:bg-purple-800 transition' href='https://twitch.tv/midudev'>
-            <TwitchIcon className='w-7 h-7' /> Sígueme en Twitch
+        <div className='flex max-w-xl m-auto gap-x-2'>
+          <a
+            className='flex items-center justify-center w-[250px] p-2 font-bold text-white bg-purple-700 rounded-full gap-x-2 hover:bg-purple-500 focus:border-purple-800 active:bg-purple-800 transition'
+            href='https://twitch.tv/midudev'
+          >
+            <TwitchIcon className='w-6 h-6' /> Sígueme en Twitch
           </a>
-          <a className='flex items-center justify-center w-[250px] p-2 font-bold text-white bg-black rounded-full gap-x-2 hover:bg-black/50 ' href='https://twitch.tv/midudev'>
-            <GitHubIcon className='w-7 h-7' /> Ver código en GitHub
+          <a
+            className='flex items-center justify-center w-[250px] p-2 font-bold text-white bg-black rounded-full gap-x-2 hover:bg-black/50 '
+            href='https://twitch.tv/midudev'
+          >
+            <GitHubIcon className='w-6 h-6' /> Ver código en GitHub
           </a>
         </div>
 
+        <h3 className='flex items-center justify-center mt-8 mb-2 text-lg gap-x-2'><SunIcon />Examples</h3>
+
+        <ul className='flex flex-col gap-3.5 w-full sm:max-w-md m-auto'>
+          {
+            EXAMPLES.map((example, index) => (
+              <button key={index} onClick={() => sendPrompt({ prompt: example })} className='w-full p-3 rounded-md bg-gray-50 dark:bg-white/5 hover:bg-gray-200 dark:hover:bg-gray-900'>
+                {example} →
+              </button>
+            ))
+          }
+        </ul>
       </div>
     </section>
   )
