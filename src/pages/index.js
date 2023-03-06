38f3d38179c8975a6775f0efacf9bdff13e170e1
Miguel Ángel Durán <miduga@gmail.com>
2023-03-06 21:28:46 +0100

    Remove Banner and render messages correctly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import { Banner } from '@/components/Banner'
 import { ChatForm } from '@/components/CreatePrompt'
 import { Layout } from '@/components/Layout'
 import { Message } from '@/components/Message'
 import { Welcome } from '@/components/Welcome'
 import { useConversationsStore } from '@/store/conversations.js'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 function Chat() {
   const selectedConversation = useConversationsStore(
@@ -13,16 +13,28 @@ function Chat() {
     const { selectedConversation } = state
     return state.conversationsMessages[selectedConversation]
   })
+  const [animationParent] = useAutoAnimate()
 
   const renderContent = () => {
     if (!selectedConversation) return <Welcome />
-    return messages?.map((entry) => <Message key={entry.id} {...entry} />)
+    return (
+      <div className='flex-1 overflow-hidden'>
+        <div className='h-full overflow-auto'>
+          {messages?.map((entry) => (
+            <Message key={entry.id} {...entry} />
+          ))}
+          <div className='flex-shrink-0 w-full h-32 md:h-48 bg-gptgray' />
+        </div>
+      </div>
+    )
   }
 
   return (
     <div className='flex flex-col flex-1 h-full pl-64'>
-      <main>{renderContent()}</main>
-      <ChatForm />
+      <main className='relative w-full' ref={animationParent}>
+        {renderContent()}
+        <ChatForm />
+      </main>
     </div>
   )
 }
@@ -30,7 +42,6 @@ function Chat() {
 export default function Home() {
   return (
     <Layout>
-      <Banner />
       <Chat />
     </Layout>
   )
