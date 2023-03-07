801e214b72ea23c6c77bf41b94c6f79a6e97db1a
Miguel Ángel Durán <miduga@gmail.com>
2023-03-07 12:03:10 +0100

    Highlight code if neccesary

--- src/components/TypingEffect.jsx
+++ src/components/TypingEffect.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from 'react'
+import 'highlight.js/styles/atom-one-dark.css'
+import hljs from 'highlight.js'
 
 const useTypingEffect = ({ text }) => {
   const [displayText, setDisplayText] = useState('')
   const [currentIndex, setCurrentIndex] = useState(0) // podríamos usar un useRef
   const [showCursor, setShowCursor] = useState(true)
+
+  useEffect(() => {
+    hljs.highlightAll()
+  }, [displayText])
 
   useEffect(() => {
     if (!text?.length) return
@@ -35,5 +41,5 @@ const useTypingEffect = ({ text }) => {
 export function TypingEffect ({ text }) {
   const { displayText, showCursor } = useTypingEffect({ text })
 
-  return <span className={`${showCursor ? 'after:content-["▋"] after:ml-1 after:animate-typing' : ''}`}>{displayText}</span>
+  return <span className={`${showCursor ? 'after:content-["▋"] after:ml-1 after:animate-typing' : ''}`} dangerouslySetInnerHTML={{ __html: displayText }} />
 }
